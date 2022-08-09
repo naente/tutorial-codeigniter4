bc5540659e3381158a8e2e0c79aca54a068d5f72
naente <naente.dev@gmail.com>
2022-08-09 10:10:20 +0900

    wip

--- ci4/app/Views/Animals/delete.php
+++ ci4/app/Views/Animals/delete.php
@@ -1,36 +1,23 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width">
-    <title>削除</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-<body>
-
-<div class="container">
-    <h1>削除</h1>
-    <p>本当に削除しますか？</p>
-    <dl>
-        <dt>ID</dt>
-        <dd><?php echo $animal['id']; ?></dd>
-        <dt>名前</dt>
-        <dd><?php echo esc($animal['name']); ?></dd>
-        <dt>作成日時</dt>
-        <dd><?php echo $animal['created_at']; ?></dd>
-        <dt>更新日時</dt>
-        <dd><?php echo $animal['updated_at']; ?></dd>
-    </dl>
-    <?php echo form_open('/animals/delete/' . $animal['id']); ?>
-    <?php echo csrf_field(); ?>
-    <a class="btn btn-secondary" href="<?php echo site_url('/animals/show/' . $animal['id']); ?>">いいえ</a>
-    <button class="btn btn-danger">はい</button>
-    </form>
-</div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
-</body>
-</html>
+<?php $this->extend('layouts/wrapper'); ?>
+<?php $this->section('title'); ?>削除<?php $this->endSection(); ?>
+<?php $this->section('content'); ?>
+    <div class="container">
+        <h1>削除</h1>
+        <p>本当に削除しますか？</p>
+        <dl>
+            <dt>ID</dt>
+            <dd><?php echo $animal['id']; ?></dd>
+            <dt>名前</dt>
+            <dd><?php echo esc($animal['name']); ?></dd>
+            <dt>作成日時</dt>
+            <dd><?php echo $animal['created_at']; ?></dd>
+            <dt>更新日時</dt>
+            <dd><?php echo $animal['updated_at']; ?></dd>
+        </dl>
+        <?php echo form_open('/animals/delete/' . $animal['id']); ?>
+        <?php echo csrf_field(); ?>
+        <a class="btn btn-secondary" href="<?php echo site_url('/animals/show/' . $animal['id']); ?>">いいえ</a>
+        <button class="btn btn-danger">はい</button>
+        </form>
+    </div>
+<?php $this->endSection(); ?>
--- ci4/app/Views/Animals/edit.php
+++ ci4/app/Views/Animals/edit.php
@@ -1,41 +1,29 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width">
-    <title>編集</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-<body>
-
-<div class="container">
-    <h1>編集</h1>
-    <?php if (session()->has('warning')) { ?>
-        <div class="alert alert-warning" role="alert">
-            <?php echo session('warning'); ?>
+<?php $this->extend('layouts/wrapper'); ?>
+<?php $this->section('title'); ?>編集<?php $this->endSection(); ?>
+<?php $this->section('content'); ?>
+    <div class="container">
+        <h1>編集</h1>
+        <?php if (session()->has('warning')) { ?>
+            <div class="alert alert-warning" role="alert">
+                <?php echo session('warning'); ?>
+            </div>
+        <?php } ?>
+        <?php if (session()->has('errors')) { ?>
+            <ul>
+                <?php foreach (session('errors') as $error) { ?>
+                    <li><?php echo $error; ?></li>
+                <?php } ?>
+            </ul>
+        <?php } ?>
+        <?php echo form_open('/animals/update/' . $animal['id']); ?>
+        <?php echo csrf_field(); ?>
+        <div class="mb-3">
+            <label for="name" class="form-label">名前</label>
+            <input type="text" class="form-control" name="name" id="name"
+                   value="<?php echo old('name', esc($animal['name'])); ?>">
         </div>
-    <?php } ?>
-    <?php if (session()->has('errors')) { ?>
-        <ul>
-            <?php foreach (session('errors') as $error) { ?>
-                <li><?php echo $error; ?></li>
-            <?php } ?>
-        </ul>
-    <?php } ?>
-    <?php echo form_open('/animals/update/' . $animal['id']); ?>
-    <?php echo csrf_field(); ?>
-    <div class="mb-3">
-        <label for="name" class="form-label">名前</label>
-        <input type="text" class="form-control" name="name" id="name" value="<?php echo old('name', esc($animal['name'])); ?>">
+        <a class="btn btn-secondary" href="<?php echo site_url('/animals/show/' . $animal['id']); ?>">戻る</a>
+        <button class="btn btn-primary">更新</button>
+        </form>
     </div>
-    <a class="btn btn-secondary" href="<?php echo site_url('/animals/show/' . $animal['id']); ?>">戻る</a>
-    <button class="btn btn-primary">更新</button>
-    </form>
-</div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
-</body>
-</html>
+<?php $this->endSection(); ?>
